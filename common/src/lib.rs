04de14dac39e219449f806e907daa50bd5205a6c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-05-23 14:45:42 +0200

    Make Hash::from_bytes public

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -18,17 +18,17 @@ impl Hash {
     /// Length of the hash in bytes.
     pub const LENGTH: usize = SHA512_OUTPUT_LEN;
 
-    /// Converts a slice of bytes of length `HASH_LENGTH` into a `Hash`.
+    /// Converts a slice of bytes of length `Hash::LENGTH` into `&Hash`.
     ///
     /// # Panics
     ///
-    /// This will panic if `hash` is not of length `HASH_LENGTH`.
-    fn from_bytes<'a>(hash: &'a [u8]) -> &'a Hash {
+    /// This will panic if `hash` is not of length `Hash::LENGTH`.
+    pub fn from_bytes<'a>(hash: &'a [u8]) -> &'a Hash {
         assert_eq!(hash.len(), Hash::LENGTH);
         unsafe {
             // This is safe because we ensured above that `hash` is of length
-            // `HASH_LENGTH`.
-            //&*(hash.as_ptr() as *const [_; HASH_LENGTH])
+            // `Hash::LENGTH` and because `Hash` has the same layout as `[u8;
+            // Hash::LENGTH]` because we use the `repr(transparent)` attribute.
             &*(hash.as_ptr() as *const Hash)
         }
     }
