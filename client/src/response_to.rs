60eac08353dab04a9a7f3e4ed28d22a75a81ba77
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-05-24 18:06:51 +0200

    Derive Debug, Eq, Partial for response_to types

--- client/src/response_to.rs
+++ client/src/response_to.rs
@@ -7,12 +7,14 @@
 use crate::Hash;
 
 /// Response to a store request.
+#[derive(Debug, Eq, PartialEq)]
 pub enum Store<'a>{
     /// The value was successfully stored.
     Success(&'a Hash),
 }
 
 /// Response to a retrieve request.
+#[derive(Debug, Eq, PartialEq)]
 pub enum Retrieve<'a> {
     /// Value was found.
     Value(&'a [u8]),
@@ -21,6 +23,7 @@ pub enum Retrieve<'a> {
 }
 
 /// Response to a remove request.
+#[derive(Debug, Eq, PartialEq)]
 pub enum Remove {
     /// Value was successfully removed.
     Ok,
