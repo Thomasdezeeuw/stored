90e27a009ea0f2b91528588579234c31fc70c774
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-06-12 19:23:39 +0200

    Add some initial request handling to the server

--- server/src/listener.rs
+++ server/src/listener.rs
@@ -1,14 +1,16 @@
-use std::net::SocketAddr;
 use std::io;
+use std::net::SocketAddr;
+use std::sync::Arc;
 
-use futures::io::{AsyncReadExt, AsyncWriteExt};
+use futures::io::AsyncReadExt;
 use heph::log::REQUEST_TARGET;
 use heph::net::tcp::{TcpListener, TcpListenerError, TcpStream};
 use heph::system::options::Priority;
 use heph::{actor, NewActor, ActorOptions, ActorSystemRef, SupervisorStrategy};
 use log::{error, info};
 
-use crate::Key;
+use coeus_common::{parse, Request, Response};
+
 use crate::cache::CacheRef;
 
 /// Options used in [`setup`].
@@ -56,7 +58,36 @@ async fn conn_actor(
     let bytes = stream.read(&mut buf).await?;
     let buf = &buf[0..bytes];
 
-    stream.write_all(&buf).await?;
+    match parse::request(&buf) {
+        Ok((request, _n)) => {
+            match request {
+                Request::Store(value) => {
+                    let key = cache.async_add(Arc::from(value));
+                    Response::Store(&key).write_to(&mut stream).await?;
+                },
+                Request::StreamStore { .. } =>
+                    return unimplemented_err("TOOD: handle request"),
+                Request::Retrieve(key) => {
+                    if let Some(value) = cache.get(key) {
+                        Response::Value(&*value).write_to(&mut stream).await?;
+                    } else {
+                        Response::ValueNotFound.write_to(&mut stream).await?;
+                    }
+                },
+                Request::Remove(key) => {
+                    cache.async_remove(key.clone());
+                    Response::Ok.write_to(&mut stream).await?;
+                },
+            }
+        },
+        Err(_err) => return unimplemented_err("TOOD: handle parsing error"),
+    }
+
+    // TODO: handle more the 1 request.
 
     Ok(())
+}
+
+fn unimplemented_err(msg: &'static str) -> io::Result<()> {
+    Err(io::Error::new(io::ErrorKind::Other, msg))
 }
