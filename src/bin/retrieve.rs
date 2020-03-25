312b580f0b7ed2ff28cb8f0dbbd0f4eafddcaa92
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-03-25 15:20:56 +0100

    Implement store, retrieve and remove commands

--- src/bin/retrieve.rs
+++ src/bin/retrieve.rs
@@ -1,3 +1,90 @@
-fn main() {
-    unimplemented!("retrieve")
+//! Retrieve a blob.
+//!
+//! Usage:
+//!
+//!
+//! ```bash
+//! $ retrieve "$key"
+//!
+//! $ echo "$key" | retrieve
+//! ```
+
+#![feature(bool_to_option)]
+
+use std::io::{self, Read, Write};
+use std::net::{SocketAddr, TcpStream};
+use std::{env, fmt};
+
+fn main() -> Result<(), Error> {
+    // TODO: make the address configurable.
+    let address: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let mut stream = TcpStream::connect(address)?;
+
+    // Read the blob we need to store. Uses the first argument passed (if not
+    // "-") or reads from standard in.
+    let key = env::args()
+        .skip(1)
+        .next()
+        .and_then(|arg| (arg != "-").then_some(arg));
+    let key = if let Some(key) = key {
+        key
+    } else {
+        let mut buf = String::new();
+        io::stdin().read_to_string(&mut buf)?;
+        buf
+    };
+
+    let mut request = Vec::new();
+    write!(&mut request, "GET /blob/{} HTTP/1.1\r\n\r\n", key)?;
+    stream.write_all(&request)?;
+    stream.flush()?;
+
+    // Reuse the buffer.
+    let mut buf = request;
+    buf.clear();
+
+    // Read the response.
+    stream.read_to_end(&mut buf)?;
+
+    let mut headers = [httparse::EMPTY_HEADER; 5];
+    let mut resp = httparse::Response::new(&mut headers);
+    let headers_length = resp.parse(&buf)?.unwrap();
+
+    if resp.code != Some(200) {
+        let msg = String::from_utf8_lossy(&buf[headers_length..]).into_owned();
+        return Err(Error::StatusCode(resp.code.unwrap(), msg));
+    }
+
+    io::stdout()
+        .write_all(&buf[headers_length..])
+        .map_err(Into::into)
+}
+
+enum Error {
+    Io(io::Error),
+    Parse(httparse::Error),
+    StatusCode(u16, String),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<httparse::Error> for Error {
+    fn from(err: httparse::Error) -> Error {
+        Error::Parse(err)
+    }
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Error::*;
+        match self {
+            Io(err) => write!(f, "I/O error: {}", err),
+            Parse(err) => write!(f, "HTTP parsing error: {}", err),
+            StatusCode(code, msg) => write!(f, "Unexpected HTTP status code: {}: {}", code, msg),
+        }
+    }
 }
--- src/bin/store.rs
+++ src/bin/store.rs
@@ -1,3 +1,108 @@
-fn main() {
-    unimplemented!("store")
+//! Store a blob.
+//!
+//! Usage:
+//!
+//! ```bash
+//! $ store "Hello world"
+//!
+//! $ echo "Hello world" | store
+//! ```
+
+#![feature(bool_to_option)]
+
+use std::io::{self, IoSlice, Read, Write};
+use std::net::{SocketAddr, TcpStream};
+use std::os::unix::ffi::OsStringExt;
+use std::{env, fmt};
+
+fn main() -> Result<(), Error> {
+    // TODO: make the address configurable.
+    let address: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let mut stream = TcpStream::connect(address)?;
+
+    // Read the blob we need to store. Uses the first argument passed (if not
+    // "-") or reads from standard in.
+    let blob = env::args_os()
+        .skip(1)
+        .next()
+        .and_then(|arg| (arg != "-").then_some(arg));
+    let blob = if let Some(blob) = blob {
+        blob.into_vec()
+    } else {
+        let mut buf = Vec::new();
+        io::stdin().read_to_end(&mut buf)?;
+        buf
+    };
+
+    let mut headers = Vec::new();
+    write!(
+        &mut headers,
+        "POST /blob HTTP/1.1\r\nContent-Length: {}\r\n\r\n",
+        blob.len()
+    )?;
+
+    let bufs = &[IoSlice::new(&headers), IoSlice::new(&blob)];
+    stream.write_vectored(bufs)?;
+    stream.flush()?;
+
+    // Reuse the largest buffer.
+    let mut buf = if headers.len() > blob.len() {
+        headers
+    } else {
+        blob
+    };
+    buf.clear();
+
+    // Read the response.
+    stream.read_to_end(&mut buf)?;
+
+    let mut headers = [httparse::EMPTY_HEADER; 5];
+    let mut resp = httparse::Response::new(&mut headers);
+    let headers_length = resp.parse(&buf)?.unwrap();
+
+    if resp.code != Some(201) {
+        let msg = String::from_utf8_lossy(&buf[headers_length..]).into_owned();
+        return Err(Error::StatusCode(resp.code.unwrap(), msg));
+    }
+
+    let location = resp.headers.iter().find(|header| header.name == "Location");
+    if let Some(location) = location {
+        // Remove "/blob/".
+        io::stdout()
+            .write_all(&location.value[6..])
+            .map_err(Into::into)
+    } else {
+        return Err(Error::LocationHeader);
+    }
+}
+
+enum Error {
+    Io(io::Error),
+    Parse(httparse::Error),
+    StatusCode(u16, String),
+    LocationHeader,
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<httparse::Error> for Error {
+    fn from(err: httparse::Error) -> Error {
+        Error::Parse(err)
+    }
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Error::*;
+        match self {
+            Io(err) => write!(f, "I/O error: {}", err),
+            Parse(err) => write!(f, "HTTP parsing error: {}", err),
+            StatusCode(code, msg) => write!(f, "Unexpected HTTP status code: {}: {}", code, msg),
+            LocationHeader => write!(f, "Missing 'Location' header"),
+        }
+    }
 }
