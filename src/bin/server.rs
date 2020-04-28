3952783f4740d9c098488e6da42e298f8074d477
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-04-28 18:31:43 +0200

    Add --version and --help commands to the stored binary

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,28 +1,50 @@
-use std::{env, io};
+#![feature(process_exitcode_placeholder)]
 
-use heph::{Runtime, RuntimeError};
-use log::info;
+use std::env;
+use std::process::ExitCode;
+
+use heph::Runtime;
+use log::{error, info};
 
 use stored::config::Config;
 use stored::{db, http};
 
-fn main() -> Result<(), RuntimeError<io::Error>> {
-    heph::log::init();
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const USAGE: &str = "Usage:
+    \tstored <path_to_config>
+    \tstored -v or --version
+    \tstored -h or --help";
 
-    let config_path = env::args().nth(1).ok_or_else(|| {
-        eprintln!("Missing path to configuration file.\nUsage:\n\tstored <path_to_config>");
-        io::Error::new(io::ErrorKind::Other, "missing path to configuration file")
+fn main() -> ExitCode {
+    heph::log::init();
+    let code = match try_main() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(code) => code,
+    };
+    log::logger().flush();
+    return code;
+}
+
+fn try_main() -> Result<(), ExitCode> {
+    let config_path = parse_args()?;
+    let config = Config::from_file(&config_path).map_err(|err| {
+        error!("error opening configuration file: {}", err);
+        ExitCode::FAILURE
     })?;
-    let config = Config::from_file(&config_path)?;
 
     let mut runtime = Runtime::new().use_all_cores();
 
     info!("opening database '{}'", config.path.display());
-    let db_ref = db::start(&mut runtime, config.path)?;
+    let db_ref = db::start(&mut runtime, config.path).map_err(|err| {
+        error!("error opening database: {}", err);
+        ExitCode::FAILURE
+    })?;
 
-    // Setup our HTTP server.
     info!("listening on http://{}", config.http.address);
-    let start_listener = http::setup(config.http.address, db_ref)?;
+    let start_listener = http::setup(config.http.address, db_ref).map_err(|err| {
+        error!("error binding HTTP server: {}", err);
+        ExitCode::FAILURE
+    })?;
 
     if let Some(distributed_config) = config.distributed {
         info!(
@@ -36,4 +58,21 @@ fn main() -> Result<(), RuntimeError<io::Error>> {
     runtime
         .with_setup(move |mut runtime_ref| start_listener(&mut runtime_ref))
         .start()
+        .map_err(|err| {
+            error!("{}", err);
+            ExitCode::FAILURE
+        })
+}
+
+/// Parses the arguments.
+/// Handles `-v` & `--version`, `-h` & `--help` and unknown arguments.
+fn parse_args() -> Result<String, ExitCode> {
+    match env::args().nth(1) {
+        Some(arg) if arg == "-v" || arg == "--version" => eprintln!("stored v{}", VERSION),
+        Some(arg) if arg == "-h" || arg == "--help" => eprintln!("{}", USAGE),
+        Some(arg) if arg.starts_with("-") => eprintln!("unknown argument '{}'.\n\n{}", arg, USAGE),
+        Some(config_path) => return Ok(config_path),
+        None => eprintln!("missing path to configuration file.\n\n{}", USAGE),
+    }
+    Err(ExitCode::FAILURE)
 }
