abcdd901f01203fece166ec7a4d19bd4354c09c1
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-04-29 11:18:28 +0200

    Add description of Stored to --help command

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Store*d*
 
-Store*d* (pronounced store-daemon, or just stored) is a distributed immutable
-value store. Stored is not a key-value store, as the key isn't the decided by
-the user but by the checksum of the stored value.
 
-It supports three operations: storing, retrieving and removing values. As the
-key of a value is its checksum it not possible to modify values. If a value
-needs to be modified a new value simply needs to stored and the new key used.
-The client can validate the correct delivery of the value by using the returned
-key (checksum). They values themselves are unchanged by Stored and are seen as
-binary blobs.
+Store*d* (pronounced store-daemon, or just stored) is a distributed immutable
+blob store. Stored is not a key-value store, as the key isn't the decided by the
+user but by the SHA-512 checksum of the stored blob.
+
+It supports three operations: storing, retrieving and removing blobs. As the key
+of a blob is its checksum it is not possible to modify blobs. If a blob needs to
+be modified a new blob simply needs to be stored and the new key used. The
+client can validate the correct delivery of the blob by using the returned key
+(checksum). The blob themselves are unchanged by Stored.
 
 
 ## Operations
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,10 +10,26 @@ use stored::config::Config;
 use stored::{db, http};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-const USAGE: &str = "Usage:
+const USAGE: &str = concat!(
+    "Stored v",
+    env!("CARGO_PKG_VERSION"),
+    "
+
+Stored (pronounced store-daemon, or just stored) is a distributed immutable blob
+store. Stored is not a key-value store, as the key isn't the decided by the user
+but by the SHA-512 checksum of the stored blob.
+
+It supports three operations: storing, retrieving and removing blobs. As the key
+of a blob is its checksum it is not possible to modify blobs. If a blob needs to
+be modified a new blob simply needs to be stored and the new key used. The
+client can validate the correct delivery of the blob by using the returned key
+(checksum). The blob themselves are unchanged by Stored.
+
+Usage:
     \tstored <path_to_config>
     \tstored -v or --version
-    \tstored -h or --help";
+    \tstored -h or --help"
+);
 
 fn main() -> ExitCode {
     heph::log::init();
@@ -68,11 +84,22 @@ fn try_main() -> Result<(), ExitCode> {
 /// Handles `-v` & `--version`, `-h` & `--help` and unknown arguments.
 fn parse_args() -> Result<String, ExitCode> {
     match env::args().nth(1) {
-        Some(arg) if arg == "-v" || arg == "--version" => eprintln!("stored v{}", VERSION),
-        Some(arg) if arg == "-h" || arg == "--help" => eprintln!("{}", USAGE),
-        Some(arg) if arg.starts_with("-") => eprintln!("unknown argument '{}'.\n\n{}", arg, USAGE),
-        Some(config_path) => return Ok(config_path),
-        None => eprintln!("missing path to configuration file.\n\n{}", USAGE),
+        Some(arg) if arg == "-v" || arg == "--version" => {
+            println!("Stored v{}", VERSION);
+            Err(ExitCode::SUCCESS)
+        }
+        Some(arg) if arg == "-h" || arg == "--help" => {
+            println!("{}", USAGE);
+            Err(ExitCode::SUCCESS)
+        }
+        Some(arg) if arg.starts_with("-") => {
+            eprintln!("unknown argument '{}'.\n\n{}", arg, USAGE);
+            Err(ExitCode::FAILURE)
+        }
+        Some(config_path) => Ok(config_path),
+        None => {
+            eprintln!("missing path to configuration file.\n\n{}", USAGE);
+            Err(ExitCode::FAILURE)
+        }
     }
-    Err(ExitCode::FAILURE)
 }
