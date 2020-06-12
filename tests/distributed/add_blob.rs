b022a3c52e0240121df3cecf62d5d95bce00b21b
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-06-12 17:33:47 +0200

    Add some more distributed tests for adding blob

--- tests/distributed/add_blob.rs
+++ tests/distributed/add_blob.rs
@@ -22,9 +22,8 @@ const FILTER: LevelFilter = LevelFilter::Warn;
 macro_rules! tests {
     (
         $(
-        $( #[$meta: meta] )*
-        fn $name: ident ()
-            $body: block
+            $( #[$meta: meta] )*
+            fn $name: ident () $body: block
         )+
     ) => {
         mod with_2_peers {
@@ -41,10 +40,8 @@ macro_rules! tests {
             );
 
             $(
-            $( #[$meta] )*
-            fn $name() {
-                $body
-            }
+                $( #[$meta] )*
+                fn $name() $body
             )+
         }
 
@@ -84,6 +81,105 @@ tests! {
                 LAST_MODIFIED => &last_modified,
                 CONNECTION => header::KEEP_ALIVE,
             );
+        }
+    }
+
+    #[test]
+    fn store_hello_mars_twice_same_node() {
+        let _p = start_stored();
+
+        let url = "/blob/b09bcc84b88e440dad90bb19baf0c0216d8929baebc785fa0e387a17c46fe131f45109b5f06a632781c5ecf1bf1257c205bbea6d3651a9364a7fc6048cdc155c";
+        let blob = b"Hello mars";
+        let blob_len = "10";
+        let post_port = DB_PORTS[1];
+        request!(
+            POST post_port, "/blob", blob,
+            CONTENT_LENGTH => blob_len;
+            expected: StatusCode::CREATED, body::EMPTY,
+            CONTENT_LENGTH => body::EMPTY_LEN,
+            LOCATION => url,
+            CONNECTION => header::KEEP_ALIVE,
+        );
+        let last_modified = date_header();
+
+        for port in DB_PORTS.iter().copied() {
+            request!(
+                GET port, url, body::EMPTY,
+                expected: StatusCode::OK, b"Hello world",
+                CONTENT_LENGTH => "11",
+                LAST_MODIFIED => &last_modified,
+                CONNECTION => header::KEEP_ALIVE,
+            );
+        }
+
+        // Storing the blob a second time shouldn't change anything.
+        request!(
+            POST post_port, "/blob", blob,
+            CONTENT_LENGTH => blob_len;
+            expected: StatusCode::CREATED, body::EMPTY,
+            CONTENT_LENGTH => body::EMPTY_LEN,
+            LOCATION => url,
+            CONNECTION => header::KEEP_ALIVE,
+        );
+
+        for port in DB_PORTS.iter().copied() {
+            request!(
+                GET port, url, body::EMPTY,
+                expected: StatusCode::OK, b"Hello world",
+                CONTENT_LENGTH => "11",
+                LAST_MODIFIED => &last_modified,
+                CONNECTION => header::KEEP_ALIVE,
+            );
+        }
+    }
+
+    #[test]
+    fn store_hello_moon_multiple_times_different_nodes() {
+        let _p = start_stored();
+
+        let url = "/blob/03ae65afb6e91c5668faf2c829de80160b79c812555f52c90693df7d9489114adfb85191084e3a816ec4e36870ddf4fbe84c03cd4006bcd705a983f755f4a6f7";
+        let blob = b"Hello moon";
+        let blob_len = "10";
+        request!(
+            POST DB_PORTS[0], "/blob", blob,
+            CONTENT_LENGTH => blob_len;
+            expected: StatusCode::CREATED, body::EMPTY,
+            CONTENT_LENGTH => body::EMPTY_LEN,
+            LOCATION => url,
+            CONNECTION => header::KEEP_ALIVE,
+        );
+        let last_modified = date_header();
+
+        for port in DB_PORTS.iter().copied() {
+            request!(
+                GET port, url, body::EMPTY,
+                expected: StatusCode::OK, b"Hello world",
+                CONTENT_LENGTH => "11",
+                LAST_MODIFIED => &last_modified,
+                CONNECTION => header::KEEP_ALIVE,
+            );
+        }
+
+        // Storing the blob a second time shouldn't change anything.
+        for post_port in DB_PORTS.iter().copied().skip(1) {
+            request!(
+                POST post_port, "/blob", blob,
+                CONTENT_LENGTH => blob_len;
+                expected: StatusCode::CREATED, body::EMPTY,
+                CONTENT_LENGTH => body::EMPTY_LEN,
+                LOCATION => url,
+                CONNECTION => header::KEEP_ALIVE,
+            );
+
+            for get_port in DB_PORTS.iter().copied() {
+                request!(
+                    GET get_port, url, body::EMPTY,
+                    expected: StatusCode::OK, b"Hello world",
+                    CONTENT_LENGTH => "11",
+                    LAST_MODIFIED => &last_modified,
+                    CONNECTION => header::KEEP_ALIVE,
+                );
+            }
         }
     }
 }
