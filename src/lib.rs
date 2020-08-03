1d7e23a47dbc98721e0430405ecbea377c28e6ef
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-08-03 12:15:46 +0200

    Remove unused unstable features

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 #![feature(
     bool_to_option,
-    box_into_raw_non_null,
-    drain_filter,
     exact_size_is_empty,
     hash_set_entry,
     maybe_uninit_slice,
     maybe_uninit_slice_assume_init,
-    maybe_uninit_uninit_array,
     never_type,
-    result_flattening,
     type_alias_impl_trait
 )]
 
