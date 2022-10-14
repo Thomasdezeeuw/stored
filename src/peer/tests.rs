d67b48f8315ac2dafadcf2eff5a8772b6044746b
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2022-10-14 18:22:06 +0200

    Nuke.
    Complete rewrite.

--- src/peer/tests.rs
+++
@@ -1,7 +0,0 @@
-use crate::peer::{COORDINATOR_MAGIC, MAGIC_LENGTH, PARTICIPANT_MAGIC};
-
-#[test]
-fn magic_bytes() {
-    assert_eq!(MAGIC_LENGTH, COORDINATOR_MAGIC.len());
-    assert_eq!(MAGIC_LENGTH, PARTICIPANT_MAGIC.len());
-}
