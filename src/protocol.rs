bf03675fb57622c1c40b25aa427e0db19c44213d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2022-10-17 22:39:00 +0200

    Add initial version of the protocol module
    It contains the Protocol trait which defines how to interact with
    connected clients. With this is should be easier to support multiple
    protocol, such as HTTP and RESP, within the same implementation.

---
+++ src/protocol.rs
@@ -0,0 +1,59 @@
+//! Define how to interact with connected clients.
+
+use std::fmt;
+use std::future::Future;
+
+use crate::key::Key;
+use crate::storage::Blob;
+
+/// Protocol that defines how to interact with connected clients.
+pub trait Protocol {
+    type Error: fmt::Display;
+
+    /// Read the next request.
+    fn next_request<'a>(&'a mut self) -> Self::NextRequest<'a>;
+
+    /// [`Future`] behind [`Protocol::next_request`].
+    type NextRequest<'a>: Future<Output = Result<Option<Request<'a>>, Self::Error>> + 'a
+    where
+        Self: 'a;
+
+    /// Reply to a request with `response`.
+    fn reply<'a, B>(&'a mut self, response: Response<B>) -> Self::Reply<'a>
+    where
+        B: Blob;
+
+    /// [`Future`] behind [`Protocol::reply`].
+    type Reply<'a>: Future<Output = Result<(), Self::Error>> + 'a
+    where
+        Self: 'a;
+}
+
+/// Request read by a [`Protocol`] implementation.
+pub enum Request<'a> {
+    /// Add a blob to the storage.
+    AddBlob(&'a [u8]),
+    /// Remove the blob with key.
+    RemoveBlob(Key),
+    /// Get blob with key.
+    GetBlob(Key),
+    /// Check if a blob with key exists.
+    CointainsBlob(Key),
+}
+
+/// Response to a [`Request`], generic over the blob type `B`.
+pub enum Response<B: Blob> {
+    /// Blob has been added.
+    Added,
+    /// Blob has been removed.
+    BlobRemoved,
+    /// Blob was retrieved.
+    Blob(B),
+    /// Store contains the blob.
+    ContainsBlob,
+    /// Blob is not found, e.g. when removing or getting it.
+    BlobNotFound,
+    /// Server error occured, no detail is specified, but an error is logged.
+    /// This is not an error from normal processing, something bad happened.
+    Error,
+}
