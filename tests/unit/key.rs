af72f83d92750065f89e0cdee2d3f13df97c42dd
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-03-27 10:24:11 +0100

    Add key! macro to create a Key
    Useful in tests.

--- tests/unit/key.rs
+++ tests/unit/key.rs
@@ -1,7 +1,7 @@
 use std::hash::{Hash, Hasher};
 use std::io::{self, Read};
 
-use stored::key::{InvalidKeyStr, Key, KeyHasher};
+use stored::key::{key, InvalidKeyStr, Key, KeyHasher};
 
 #[test]
 fn to_owned() {
@@ -95,4 +95,11 @@ fn key_hashing() {
         key.hash(&mut hasher);
         hasher.finish()
     }
+}
+
+#[test]
+fn key_macro() {
+    let key = key!("b7f783baed8297f0db917462184ff4f08e69c2d5e5f79a942600f9725f58ce1f29c18139bf80b06c0fff2bdd34738452ecf40c488c22a7e3d80cdf6f9c1c0d47");
+    let expected = "b7f783baed8297f0db917462184ff4f08e69c2d5e5f79a942600f9725f58ce1f29c18139bf80b06c0fff2bdd34738452ecf40c488c22a7e3d80cdf6f9c1c0d47";
+    assert_eq!(format!("{key}"), expected);
 }
